6315df5b1a01f19f8d596ecede0c33787f311581
TMThaor <trieuthao115@gmail.com>
2025-07-24 02:22:00 +0700

    update CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
